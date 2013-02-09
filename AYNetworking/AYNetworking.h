5faa360bc1fba86fdf6b74dbe12921eb563d0137
Jan Böhler <boehler.jan@gmail.com>
2013-02-09 12:57:45 +0100

    Initial Commit

---
+++ AYNetworking/AYNetworking.h
@@ -0,0 +1,13 @@
+//
+//  AYNetworking.h
+//  AYNetworking
+//
+//  Created by Jan Boehler on 09.02.13.
+//  Copyright (c) 2013 Jan Böhler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AYNetworking : NSObject
+
+@end
---
+++ AYNetworking/AYNetworking.m
@@ -0,0 +1,13 @@
+//
+//  AYNetworking.m
+//  AYNetworking
+//
+//  Created by Jan Boehler on 09.02.13.
+//  Copyright (c) 2013 Jan Böhler. All rights reserved.
+//
+
+#import "AYNetworking.h"
+
+@implementation AYNetworking
+
+@end
